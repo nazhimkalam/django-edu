342a0e4bf68ffbe26e9a823745cd182cbd4342f6
Nazhim <nazhimkalam@gmail.com>
2021-06-02 12:46:26 +0530

    worked with GET and POST request

--- telusko/calc/views.py
+++ telusko/calc/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'name': 'Nazhim'})
+
+def add(request):
+    val_1 = int(request.POST['num1'])
+    val_2 = int(request.POST['num2'])
+    res = val_1 + val_2
+    
+    return render(request, 'result.html', {'result': res})
